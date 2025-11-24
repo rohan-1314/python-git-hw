1a99aa3e7cfd77bb198dede7e5686111d35c0f99
Rohan <rohan.vagadiya14@gmail.com>
2025-11-23 23:01:14 -0500

    error handling anf convert input

--- app.py
+++ app.py
@@ -3,3 +3,12 @@ print("Welcome to my Python program!")
 
 # Task 2: Ask the user for input
 hours = input("How many hours did you study today? ")
+
+# Task 5 (placed early so it wraps conversion + Task 3):
+# Add basic error handling in case user enters non-numeric input
+try:
+    # Task 3: Convert input to a number
+    hours = float(hours)
+except ValueError:
+    print("Please enter a valid number next time.")
+    exit()
